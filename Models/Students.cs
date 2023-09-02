84fd3c6be9b69f9345f02a28e207422c8bea19d1
2Sravs <sravani.chekuri1243#gmail.com>
2023-09-02 19:12:43 +0530

    firstcommit

---
+++ Models/Students.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Phase3Section2._14.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+       
+        public string Address { get; set; }
+
+       
+        public string Class { get; set; }
+
+        public string ContactEmail { get; set; }
+
+       
+        public int Age { get; set; }
+    }
+}
